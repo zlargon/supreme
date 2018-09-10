38018fa697642e297130b8429e857dc0952d57bf
zlargon <zlargon1988@gmail.com>
2018-09-10 15:14:40 -0400

    add index and recaptcha html

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.sendFile('./public/index.html', { root: __dirname + '/..'})
+});
+
+router.get('/recaptcha', (req, res, next) => {
+  res.sendFile('./public/recaptcha.html', { root: __dirname + '/..'})
 });
 
 module.exports = router;
