834b70945977239c6c5213de92b438c3b320f413
zlargon <zlargon1988@gmail.com>
2018-09-10 15:19:33 -0400

    implement recaptcha client

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.sendFile('./public/index.html', { root: __dirname + '/..'})
+  res.sendFile('./public/index.html', { root: __dirname + '/..'});
 });
 
 router.get('/recaptcha', (req, res, next) => {
-  res.sendFile('./public/recaptcha.html', { root: __dirname + '/..'})
+  res.sendFile('./public/recaptcha.html', { root: __dirname + '/..'});
+});
+
+router.post('/recaptcha', (req, res, next) => {
+  console.log(req.body);
 });
 
 module.exports = router;
