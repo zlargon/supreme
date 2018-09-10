95e8648db000be7e82d9a46cfc5ae685e895d3c0
zlargon <zlargon1988@gmail.com>
2018-09-10 16:40:20 -0400

    implement checkout callback function

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const verify = require('../lib/grecaptcha-verify');
+const secret = '6LcLVm8UAAAAAJPqEiifvZogYGv-PZ_TNZKMhUYS';
 
-/* GET home page. */
+/* index */
 router.get('/', (req, res, next) => {
   res.sendFile('./public/index.html', { root: __dirname + '/..'});
 });
 
+/* checkout */
+router.get('/checkout', (req, res, next) => {
+  res.sendFile('./public/checkout.html', { root: __dirname + '/..'});
+});
+
+router.post('/checkout.json', (req, res, next) => {
+  const response = req.body['g-recaptcha-response'];
+
+  verify(secret, response)
+    .then(data => {
+      res.status(200).json([req.body, data]);
+    });
+});
+
+/* recaptcha */
 router.get('/recaptcha', (req, res, next) => {
   res.sendFile('./public/recaptcha.html', { root: __dirname + '/..'});
 });
 
 router.post('/recaptcha', (req, res, next) => {
-  const secret = '6LcLVm8UAAAAAJPqEiifvZogYGv-PZ_TNZKMhUYS';
   const response = req.body['g-recaptcha-response'];
 
   verify(secret, response)
